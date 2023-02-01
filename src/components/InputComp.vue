02020d9f70b6bbc3577113eaf421fb3e8fe68709
Danilo <danvalerio89@gmail.com>
2023-02-01 12:00:11 -0300

    chore: fixing datepicker

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -2,35 +2,27 @@
 import { ref } from "vue";
 import Datepicker from "vue3-datepicker";
 
-const customStyle = ref({
-  "--vdp-bg-color": "#5c5c5c",
-  "--vdp-text-color": "#d9d9d9",
-  "--vdp-box-shadow":
-    "0 4px 10px 0 rgba(128, 144, 160, 0.1), 0 0 1px 0 rgba(128, 144, 160, 0.81)",
-  "--vdp-border-radius": "3px",
-  "--vdp-heading-size": "2.5em",
-  "--vdp-heading-weight": "bold",
-  "--vdp-heading-hover-color": "#eeeeee",
-  "--vdp-arrow-color": "currentColor",
-  "--vdp-elem-color": "currentColor",
-  "--vdp-disabled-color": "#d5d9e0",
-  "--vdp-hover-color": "#ffffff",
-  "--vdp-hover-bg-color": "#0baf74",
-  "--vdp-selected-color": "#ffffff",
-  "--vdp-selected-bg-color": "#0baf74",
-  "--vdp-current-date-outline-color": "#888888",
-  "--vdp-current-date-font-weight": "bold",
-  "--vdp-elem-font-size": "0.8em",
-  "--vdp-elem-border-radius": "3px",
-  "--vdp-divider-color": "currentColor",
+const inputStyle = ref({
+  display: "flex",
+  border: "2px solid white",
+  width: "100px",
+  "border-radius": "5px",
+  padding: "8px",
+  margin: "15px auto",
 });
 
-// const inputValue = ref("");
-const picked = ref(new Date());
-// const handleInput = () => {
-//   console.log(inputValue.value);
-//   inputValue.value = "";
-// };
+const inputFormat = ref("dd-MM-yyyy");
+const startDate = ref(new Date());
+const endDate = ref(new Date());
+
+const inputValue = ref("");
+
+const handleInput = () => {
+  if (startDate.value > endDate.value) {
+    return console.log("Errooooooooo");
+  }
+  return console.log("Deeeeeeeeeeeeeu");
+};
 </script>
 
 <template>
@@ -42,18 +34,37 @@ const picked = ref(new Date());
       v-model="inputValue"
       @input="(event) => (inputValue = event.target.value)"
     />
-    <button class="button" @click="handleInput">Procurar</button>
+    <button class="button" @click="handleInput()">Procurar</button>
   </v-container>
-  <v-row justify="center"> </v-row>
-  <div>
-    <Datepicker v-model="picked" :style="customStyle" />
-  </div>
+
+  <v-container class="mt-4 mb-4 mx-auto">
+    <v-row align="center" justify="center" class="w-100">
+      <v-col cols="6">
+        <p class="text-center text-left">
+          De: {{ startDate.toLocaleDateString("pt-BR") }}
+        </p>
+        <Datepicker
+          v-model="startDate"
+          :style="inputStyle"
+          :inputFormat="inputFormat"
+        />
+      </v-col>
+
+      <v-col cols="6">
+        <p class="text-center">
+          Até: {{ endDate.toLocaleDateString("pt-BR") }}
+        </p>
+        <Datepicker v-model="endDate" :style="inputStyle" />
+      </v-col>
+    </v-row>
+
+    <v-divider></v-divider>
+  </v-container>
 </template>
 
 <style scoped>
 .color {
-  color: black;
-  background-color: blue;
+  color: white;
 }
 .input {
   border: 1px solid red;
